66a906bcb6470b793b76030b2e0a24a7f5e4f9b3
shirawein <sbmw15@gmail.com>
2022-08-23 12:24:15 -0400

    Update x_evaluation-fixed-s2match.sh

--- s2match/x_evaluation-fixed-s2match.sh
+++ s2match/x_evaluation-fixed-s2match.sh
@@ -9,19 +9,19 @@ TAU="0.5"
 DIFFSENSE="0.5"
 
 # Evaluation script. Run as: ./evaluation.sh <parsed_data> <gold_data>
-out=`python smatch/mbert_s2match.py --pr -f "$1" "$2" -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
+out=`python smatch/xs2match.py --pr -f "$1" "$2" -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
 out=($out)
 echo 'S2match -> P: '${out[1]}', R: '${out[3]}', F: '${out[6]} | sed 's/.$//'
 
 python unlabel.py "$1"  > 1.tmp
 python unlabel.py "$2"  > 2.tmp
-out=`python smatch/mbert_s2match.py --pr -f 1.tmp 2.tmp -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
+out=`python smatch/xs2match.py --pr -f 1.tmp 2.tmp -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
 out=($out)
 echo 'Unlabeled -> P: '${out[1]}', R: '${out[3]}', F: '${out[6]} | sed 's/.$//'
 
 cat "$1" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 1.tmp
 cat "$2" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 2.tmp
-out=`python smatch/mbert_s2match.py --pr -f 1.tmp 2.tmp -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
+out=`python smatch/xs2match.py --pr -f 1.tmp 2.tmp -vectors $VECS -similarityfunction $SF -cutoff $TAU -diffsense $DIFFSENSE`
 out=($out)
 echo 'No WSD -> P: '${out[1]}', R: '${out[3]}', F: '${out[6]} | sed 's/.$//'
 
